ac1b63406e76e132260cce8a147e1555d4e775f5
pradipmengal <97036198+pradipmengal@users.noreply.github.com>
2025-06-04 14:36:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, welcome to Node.js!\n');
+  res.end('Hello, welcome to Webapp!\n');
 });
 
 server.listen(PORT, () => {
